79d900d7986d60a5d92b0d8fc0d70ba1bd21be0f
Shashwat Vikram <shashwat.vikram50@gmail.com>
2024-08-22 17:48:23 +0530

    Added basic pipeline usage example

---
+++ basic_pipeline.py
@@ -0,0 +1,24 @@
+import os
+from transformers import pipeline
+
+
+def sentimentAnalysisTask():
+    classifier = pipeline('sentiment-analysis')
+    print(classifier('We are very happy to introduce pipeline to the transformers repository.'))
+
+
+def speechRecognitionTask():
+    fffmpeg_path = "E:/Projects/GenAI/Projects/Hugging-Face-Experiments/ffmpeg-master-latest-win64-gpl-shared/bin"
+    if not os.path.exists(fffmpeg_path):
+        print("Add ffmpeg path binary")
+        return
+    os.environ['PATH'] += ";%s" % fffmpeg_path
+    transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-small.en")
+    print(transcriber('Sample Test Input.mp3'))
+
+
+if __name__ == '__main__':
+    if 0:
+        sentimentAnalysisTask()
+    else:
+        speechRecognitionTask()
