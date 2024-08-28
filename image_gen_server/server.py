0793e6d5b41c855104dd0da3858d46ec0e17448c
Shashwat Vikram <shashwat.vikram50@gmail.com>
2024-08-28 22:06:47 +0530

    Added quick image generator server using aMUSEd model

---
+++ image_gen_server/server.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from uuid import uuid4
+
+#################################################
+import os
+# Update the place where models are dumped for storage space control
+os.environ['HF_HOME'] = os.path.join(os.getcwd(), "../hf_home")
+
+from diffusers import DiffusionPipeline
+import torch
+
+pipeline = DiffusionPipeline.from_pretrained("amused/amused-512").to("cuda")
+def textToImage(prompt):
+    image = pipeline(prompt, generator=torch.Generator('cuda').manual_seed(8)).images[0]
+    return image
+#################################################
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return render_template("server.html")
+
+
+@app.route("/gen_image", methods=['POST'])
+def api():
+    prompt = request.json['prompt']
+    pil_img = textToImage(prompt)
+    file_path = "static/temp/%s.png" % uuid4().hex[:4]
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    pil_img.save(file_path, 'PNG')
+    return file_path
+
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
